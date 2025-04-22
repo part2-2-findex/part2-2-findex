4854f30eaf35df83dfdfdaa2e3c19ab4f3fbc265
Leekb0804 <lkb8713@gmail.com>
2025-04-22 10:38:42 +0900

    feat: openAPI 요청 파라미터 DTO 추가

---
+++ src/main/java/com/part2/findex/openapi/dto/StockIndexRequestParam.java
@@ -0,0 +1,37 @@
+package com.part2.findex.openapi.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class StockIndexRequestParam {
+  private int numOfRows;
+  private int pageNo;
+  private String resultType;
+  private String serviceKey;    // 필수
+
+  private String basDt;
+  private String beginBasDt;
+  private String endBasDt;
+  private String likeBasDt;
+  private String idxNm;
+  private String likeIdxNm;
+
+  private Long beginEpyItmsCnt;
+  private Long endEpyItmsCnt;
+  private Double beginFltRt;
+  private Double endFltRt;
+
+  private Long beginTrqu;
+  private Long endTrqu;
+  private Long beginTrPrc;
+  private Long endTrPrc;
+
+  private Long beginLstgMrktTotAmt;
+  private Long endLstgMrktTotAmt;
+  private Double beginLsYrEdVsFltRg;
+  private Double endLsYrEdVsFltRg;
+  private Double beginLsYrEdVsFltRt;
+  private Double endLsYrEdVsFltRt;
+}
