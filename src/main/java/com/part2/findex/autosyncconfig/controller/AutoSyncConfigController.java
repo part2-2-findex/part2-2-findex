3fe3c911fde8a32c6cf3fbc69e686231941fec67
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-04-22 18:30:20 +0900

    feat: AutoSyncConfigController 조회 메서드 추가

--- src/main/java/com/part2/findex/autosyncconfig/controller/AutoSyncConfigController.java
+++ src/main/java/com/part2/findex/autosyncconfig/controller/AutoSyncConfigController.java
@@ -2,8 +2,12 @@ package com.part2.findex.autosyncconfig.controller;
 
 import com.part2.findex.autosyncconfig.dto.AutoSyncConfigDto;
 import com.part2.findex.autosyncconfig.dto.AutoSyncConfigUpdateRequest;
+import com.part2.findex.autosyncconfig.dto.response.CursorPageResponse;
 import com.part2.findex.autosyncconfig.service.AutoSyncConfigService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +24,16 @@ public class AutoSyncConfigController {
             @RequestBody AutoSyncConfigUpdateRequest request
     ) {
         return new ResponseEntity<>(autoSyncConfigService.updateEnabled(id, request.enabled()), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<CursorPageResponse<AutoSyncConfigDto>> findAll(
+            @RequestParam(required = false) Long indexInfoId,
+            @RequestParam(required = false) Boolean enabled,
+            @RequestParam(required = false) Long idAfter,
+            @RequestParam(required = false) Long cursor,
+            @PageableDefault(size = 10, sort = "indexInfo.indexName", direction = Sort.Direction.ASC) Pageable pageable
+    ) {
+        return new ResponseEntity<>(autoSyncConfigService.findAll(indexInfoId, enabled, idAfter, cursor, pageable), HttpStatus.OK);
     }
 }
