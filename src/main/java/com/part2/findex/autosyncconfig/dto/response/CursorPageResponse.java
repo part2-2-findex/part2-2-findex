f458acc322ae14eb606a476416d100a82bd7d199
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-04-22 18:29:14 +0900

    feat: CursorPageResponse DTO 추가

---
+++ src/main/java/com/part2/findex/autosyncconfig/dto/response/CursorPageResponse.java
@@ -0,0 +1,21 @@
+package com.part2.findex.autosyncconfig.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CursorPageResponse<T> {
+    private List<T> content;
+    private String nextCursor;
+    private String nextIdAfter;
+    private Integer size;
+    private Long totalElements;
+    private boolean hasNext;
+}
