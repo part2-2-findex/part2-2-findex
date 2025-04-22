4be8341e387664c53e8f480bd4aaeb1be497d691
Leekb0804 <lkb8713@gmail.com>
2025-04-22 15:28:11 +0900

    feat: DTO 필드에 주석 추가

--- src/main/java/com/part2/findex/openapi/dto/StockIndexResponse.java
+++ src/main/java/com/part2/findex/openapi/dto/StockIndexResponse.java
@@ -5,16 +5,32 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record StockIndexResponse(Response response) {
+public record StockIndexResponse(
+    Response response
+) {
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public record Response(Header header, Body body) {}
+  public record Response(
+      Header header,
+      Body body
+  ) {}
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public record Header(String resultCode, String resultMsg) {}
+  public record Header(
+      String resultCode,    // 결과 코드
+      String resultMsg      // 결과 메세지
+  ) {}
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public record Body(int numOfRows, int pageNo, int totalCount, Items items) {}
+  public record Body(
+      int numOfRows,      // 한 페이지 결과 수
+      int pageNo,         // 페이지 번호
+      int totalCount,     // 전체 결과 수
+      Items items
+  ) {}
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public record Items(@JsonProperty("item") List<StockItem> item) {}
+  public record Items(
+      @JsonProperty("item")     // 중첩 객체 구조 + 제네릭 리스트의 경우 Jackson이 자동으로 매핑하지 못할 수 있어 작성
+      List<StockItem> item
+  ) {}
 }
--- src/main/java/com/part2/findex/openapi/dto/StockItem.java
+++ src/main/java/com/part2/findex/openapi/dto/StockItem.java
@@ -3,25 +3,25 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record StockItem(
-    String basDt,
-    String idxNm,
-    String idxCsf,
-    int epyItmsCnt,
-    double clpr,
-    double vs,
-    double fltRt,
-    double mkp,
-    double hipr,
-    double lopr,
-    long trqu,
-    long trPrc,
-    long lstgMrktTotAmt,
-    double lsYrEdVsFltRg,
-    double lsYrEdVsFltRt,
-    double yrWRcrdHgst,
-    String yrWRcrdHgstDt,
-    double yrWRcrdLwst,
-    String yrWRcrdLwstDt,
-    String basPntm,
-    double basIdx
+    String basDt,       // 기준 일자
+    String idxNm,       // 지수명
+    String idxCsf,      // 지수분류명
+    int epyItmsCnt,     // 채용종목 수
+    double clpr,        // 종가, 정규시장의 매매시간종료시까지 형성되는 최종가격
+    double vs,          // 대비, 전일 대비 등락
+    double fltRt,       // 등락률, 전일 대비 등락에 따른 비율
+    double mkp,         // 시가, 정규시장의 매매시간 개시 후 형성되는 최초가격
+    double hipr,        // 고가, 하루 중 지수의 최고치
+    double lopr,        // 저가, 하루 중 지수의 최저치
+    long trqu,          // 거래량, 지수에 포함된 종목의 거래량 총합
+    long trPrc,         // 거래대금, 지수에 포함된 종목의 거래대금 총합
+    long lstgMrktTotAmt,    // 상장시가총액, 지수에 포함된 종목의 시가총액
+    double lsYrEdVsFltRg,   // 전년말대비_등락폭, 지수의 전년말대비 등락폭
+    double lsYrEdVsFltRt,   // 전년말대비_등락률, 지수의 전년말대비 등락율
+    double yrWRcrdHgst,     // 연중기록최고, 지수의 연중최고치
+    String yrWRcrdHgstDt,   // 연중기록 최고 일자, 지수가 연중최고치를 기록한 날짜 (ex. 20240711)
+    double yrWRcrdLwst,     // 연중기록최저, 지수의 연중최저치
+    String yrWRcrdLwstDt,   // 연중기록 최저 일자, 지수가 연중최저치를 기록한 날짜 (ex. 20240801)
+    String basPntm,         // 기준시점, 지수를 산출하기 위한 기준시점 (ex. 19800104)
+    double basIdx           // 기준지수, 기준시점의 지수값
 ) {}
