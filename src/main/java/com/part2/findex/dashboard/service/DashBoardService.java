854829003e0c1dc7da1d22ca9ba7c37433dbc7f1
Leekb0804 <lkb8713@gmail.com>
2025-04-24 10:49:02 +0900

    feat: DashBoardService 인터페이스 추가

---
+++ src/main/java/com/part2/findex/dashboard/service/DashBoardService.java
@@ -0,0 +1,8 @@
+package com.part2.findex.dashboard.service;
+
+import com.part2.findex.dashboard.dto.IndexPerformanceDto;
+import java.util.List;
+
+public interface DashBoardService {
+  List<IndexPerformanceDto> getFavoriteIndexPerformance(String periodType);
+}
