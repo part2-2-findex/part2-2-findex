01df4b24b7eba8458c6d9e29a9b0878388e6677d
Leekb0804 <lkb8713@gmail.com>
2025-04-24 15:30:42 +0900

    feat: 지수차트 관련 메서드 추가

--- src/main/java/com/part2/findex/dashboard/service/DashBoardService.java
+++ src/main/java/com/part2/findex/dashboard/service/DashBoardService.java
@@ -1,8 +1,10 @@
 package com.part2.findex.dashboard.service;
 
+import com.part2.findex.dashboard.dto.IndexChartDto;
 import com.part2.findex.dashboard.dto.IndexPerformanceDto;
 import java.util.List;
 
 public interface DashBoardService {
   List<IndexPerformanceDto> getFavoriteIndexPerformance(String periodType);
+  IndexChartDto getIndexChart(Long id, String periodType);
 }
