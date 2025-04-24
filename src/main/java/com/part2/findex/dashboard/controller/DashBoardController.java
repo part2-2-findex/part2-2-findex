8203311e3b42fd4e8eb083af6174ead99203884b
Leekb0804 <lkb8713@gmail.com>
2025-04-24 11:36:08 +0900

    feat: DashBoardController 주요지수 주가현황요약 api 구현

--- src/main/java/com/part2/findex/dashboard/controller/DashBoardController.java
+++ src/main/java/com/part2/findex/dashboard/controller/DashBoardController.java
@@ -1,7 +1,10 @@
 package com.part2.findex.dashboard.controller;
 
 import com.part2.findex.dashboard.dto.IndexPerformanceDto;
+import com.part2.findex.dashboard.service.DashBoardService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/index-data")
 public class DashBoardController {
+  private final DashBoardService dashBoardService;
 
   @GetMapping("/performance/favorite")
   ResponseEntity<List<IndexPerformanceDto>> getFavoriteIndexPerformance(
       @RequestParam("periodType") String periodType
   ) {
-
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(dashBoardService.getFavoriteIndexPerformance(periodType));
   }
 }
