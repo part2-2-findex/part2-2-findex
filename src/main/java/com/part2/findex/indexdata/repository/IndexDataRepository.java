a0a846f30f0394a0d04806b3b8683eca69a22fdc
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-04-25 14:11:37 +0900

    feat: 지수 데이터 수정, 삭제 Repository 메서드 추가

--- src/main/java/com/part2/findex/indexdata/repository/IndexDataRepository.java
+++ src/main/java/com/part2/findex/indexdata/repository/IndexDataRepository.java
@@ -1,5 +1,12 @@
 package com.part2.findex.indexdata.repository;
 
+import com.part2.findex.indexdata.entity.IndexData;
+
+import java.util.Optional;
+
 public interface IndexDataRepository {
 
+    Optional<IndexData> findById(Long indexDataId);
+
+    void deleteById(Long indexDataId);
 }
