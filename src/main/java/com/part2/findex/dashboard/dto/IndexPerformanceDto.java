435f02a9ab87481df76c9502b375aebb99437a17
Leekb0804 <lkb8713@gmail.com>
2025-04-25 13:02:21 +0900

    refactor: IndexPerformanceDto dto를 record에서 class로 변경
    - JPQL에서 생성하기위해서는 생성자가 필요

--- src/main/java/com/part2/findex/dashboard/dto/IndexPerformanceDto.java
+++ src/main/java/com/part2/findex/dashboard/dto/IndexPerformanceDto.java
@@ -1,12 +1,26 @@
 package com.part2.findex.dashboard.dto;
 
-public record IndexPerformanceDto(
-    Long indexInfoId,
-    String indexClassification,
-    String indexName,
-    Double versus,
-    Double fluctuationRate,
-    Double currentPrice,
-    Double beforePrice
-) {
+import lombok.Getter;
+
+@Getter
+public class IndexPerformanceDto {
+  private Long indexInfoId;
+  private String indexClassification;
+  private String indexName;
+  private Double versus;
+  private Double fluctuationRate;
+  private Double currentPrice;
+  private Double beforePrice;
+
+  public IndexPerformanceDto(Long indexInfoId, String indexClassification, String indexName,
+      Double versus, Double fluctuationRate,
+      Double currentPrice, Double beforePrice) {
+    this.indexInfoId = indexInfoId;
+    this.indexClassification = indexClassification;
+    this.indexName = indexName;
+    this.versus = versus;
+    this.fluctuationRate = fluctuationRate;
+    this.currentPrice = currentPrice;
+    this.beforePrice = beforePrice;
+  }
 }
