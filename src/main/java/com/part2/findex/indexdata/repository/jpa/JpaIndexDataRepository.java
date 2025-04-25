0149616b366e3e310ce917041b50d37044153363
Chαnɦyeong Park <parkchanhyeong0403@gmail.com>
2025-04-25 16:01:41 +0900

    feat: 지수 데이터 수정, 삭제 (#75)
    * fix: DDL 수정
    
    * fix: 엔티티 수정
    
    * feat: 지수 데이터 수정, 삭제 Repository 메서드 추가
    
    * feat: 지수 데이터 수정, 삭제 Service 메서드 추가
    
    * feat: 지수 데이터 수정, 삭제 Controller 메서드 추가
    
    * feat: 지수 데이터 수정, DTO 추가

--- src/main/java/com/part2/findex/indexdata/repository/jpa/JpaIndexDataRepository.java
+++ src/main/java/com/part2/findex/indexdata/repository/jpa/JpaIndexDataRepository.java
@@ -1,10 +1,12 @@
 package com.part2.findex.indexdata.repository.jpa;
 
+import com.part2.findex.indexdata.entity.IndexData;
 import com.part2.findex.indexdata.repository.IndexDataRepository;
 import com.part2.findex.indexdata.repository.springjpa.SpringDataIndexDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 
 @Repository
@@ -12,5 +14,14 @@ import org.springframework.stereotype.Repository;
 public class JpaIndexDataRepository implements IndexDataRepository {
 
     private final SpringDataIndexDataRepository indexDataRepository;
+
+    public Optional<IndexData> findById(Long indexDataId) {
+        return indexDataRepository.findById(indexDataId);
+    }
+
+    @Override
+    public void deleteById(Long indexDataId) {
+        indexDataRepository.deleteById(indexDataId);
+    }
 
 }
