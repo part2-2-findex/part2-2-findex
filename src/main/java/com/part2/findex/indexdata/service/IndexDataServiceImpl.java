af6caf9f704221fc1f255f4d381bbf876d2fd11c
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-04-25 14:11:57 +0900

    feat: 지수 데이터 수정, 삭제 Service 메서드 추가

--- src/main/java/com/part2/findex/indexdata/service/IndexDataServiceImpl.java
+++ src/main/java/com/part2/findex/indexdata/service/IndexDataServiceImpl.java
@@ -1,8 +1,13 @@
 package com.part2.findex.indexdata.service;
 
+import com.part2.findex.indexdata.dto.IndexDataUpdateRequest;
+import com.part2.findex.indexdata.entity.IndexData;
 import com.part2.findex.indexdata.repository.IndexDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,19 @@ public class IndexDataServiceImpl implements IndexDataService {
 
     private final IndexDataRepository indexDataRepository;
 
+    @Transactional
+    @Override
+    public void updateIndexData(Long indexDataId, IndexDataUpdateRequest indexDataUpdateRequest) {
+        IndexData indexData = indexDataRepository.findById(indexDataId)
+                .orElseThrow(() -> new NoSuchElementException("Index data not found"));
+
+        indexData.update(indexDataUpdateRequest);
+
+    }
+
+    @Transactional
+    @Override
+    public void deleteIndexData(Long indexDataId) {
+        indexDataRepository.deleteById(indexDataId);
+    }
 }
